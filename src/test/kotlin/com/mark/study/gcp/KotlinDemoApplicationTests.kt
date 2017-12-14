11a1344f7dd79e6d8349b677aded123a1d6360e8
Mark Lee <code0x9@gmail.com>
2017-12-14 16:40:15 +0900

    ..

--- src/test/kotlin/com/mark/study/gcp/KotlinDemoApplicationTests.kt
+++ src/test/kotlin/com/mark/study/gcp/KotlinDemoApplicationTests.kt
@@ -1,16 +1,15 @@
 package com.mark.study.gcp
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@SpringBootTest()
 class KotlinDemoApplicationTests {
 
 	@Test
 	fun contextLoads() {
 	}
-
 }
